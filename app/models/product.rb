dab868a302e09630b2b05732b11ffe38f2e53c67
Carlangas <carlos@foodland.cl>
2014-10-04 17:56:10 -0300

    Modificando tablas mucho a mucho

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ActiveRecord::Base
+  has_and_belongs_to_many: promocions
+  has_and_belongs_to_many: users
   attr_accessible :marca, :modelo, :nombre, :precio
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_and_belongs_to_many: products
+  has_and_belongs_to_many: promocions
   attr_accessible :apellidoMat, :apellidoPat, :ciudad, :direccion, :nombre, :telefono
 end
  
