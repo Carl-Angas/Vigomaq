d245e09c59969a0a77594803fe883820dc3e8706
Carlangas <carlos@foodland.cl>
2014-10-04 11:07:06 -0300

    Primera Prueba OP

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prueba::Application.config.secret_token = 'b02eed9befa38c4f979fe2c00dd7ae31dcdc93dcdc7cfb7b0351200e3e7691562dd820fef5d70624d36ee2086511083e7dc5720c649bbed860468b554a12620a'
