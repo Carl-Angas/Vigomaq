4ec40d7abcc9171a03e890aa41202937a8d38272
Yao Ming <yao@localhost.localdomain>
2014-10-20 15:37:04 -0300

    Arreglando la seguridad

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -1,6 +1,7 @@
 class QueriesController < ApplicationController
   before_action :set_query, only: [:show, :edit, :update, :destroy]
-  before_filter :authenticate_user!, :except => [:show, :index]
+  before_filter :authenticate_user!
+  #, :except => [:show, :index]
   respond_to :html, :xml, :json 
 
   def index
