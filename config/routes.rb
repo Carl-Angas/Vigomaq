675320eb8bc2fd72d94202f128c5301286f2535d
Yao Ming <yao@localhost.localdomain>
2014-10-20 14:48:47 -0300

    Probando futuro template para permisos

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,16 @@ Rails.application.routes.draw do
 
   devise_for :users
   get 'inicio/index'
+  
+  #Anexando a prueba  
+  get "/inicio/prueba" => "inicio#prueba"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'inicio#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
