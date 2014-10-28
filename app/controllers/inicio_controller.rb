888e63cd3bd9c3f86e3b6644db6ebacb7dad3c1c
Yao Ming <yao@localhost.localdomain>
2014-10-28 19:00:10 -0300

    Agregando CCS a layout y a index

--- app/controllers/inicio_controller.rb
+++ app/controllers/inicio_controller.rb
@@ -1,5 +1,13 @@
 class InicioController < ApplicationController
+	respond_to :html, :xml, :json 
+
   def index
+  end
+
+  def prueba
+    @products = Product.all
+    respond_with(@products)
+
   end
   
 end
