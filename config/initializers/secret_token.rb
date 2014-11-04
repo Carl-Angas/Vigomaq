a1eb2c22e64ac950d0c78597f573434be4acc23a
Yao Ming <yao@localhost.localdomain>
2014-11-04 13:01:13 -0300

    Arreglando error cometido

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Prueba::Application.config.secret_token = 'b02eed9befa38c4f979fe2c00dd7ae31dcdc93dcdc7cfb7b0351200e3e7691562dd820fef5d70624d36ee2086511083e7dc5720c649bbed860468b554a12620a'
+# => Prueba::Application.config.secret_token = 'b02eed9befa38c4f979fe2c00dd7ae31dcdc93dcdc7cfb7b0351200e3e7691562dd820fef5d70624d36ee2086511083e7dc5720c649bbed860468b554a12620a'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141019045612) do
+ActiveRecord::Schema.define(version: 20141019024214) do
 
   create_table "categories", force: true do |t|
     t.string   "categoria"
@@ -24,6 +24,24 @@ ActiveRecord::Schema.define(version: 20141019045612) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "iniciandos", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "iniciandos", ["email"], name: "index_iniciandos_on_email", unique: true
+  add_index "iniciandos", ["reset_password_token"], name: "index_iniciandos_on_reset_password_token", unique: true
 
   create_table "model_tables", force: true do |t|
     t.string   "modelo"
@@ -44,27 +62,45 @@ ActiveRecord::Schema.define(version: 20141019045612) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "product_users", force: true do |t|
-    t.datetime "fechaEmision"
-    t.datetime "fechaVencimiento"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-  end
-
   create_table "products", force: true do |t|
     t.string   "nombre"
     t.string   "modelo"
     t.string   "marca"
     t.integer  "precio"
     t.string   "categoria"
+    t.string   "tipo"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
-    t.string   "tipo"
   end
+
+  create_table "promocions", force: true do |t|
+    t.string   "nombre"
+    t.integer  "precio"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "pruebas", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "pruebas", ["email"], name: "index_pruebas_on_email", unique: true
+  add_index "pruebas", ["reset_password_token"], name: "index_pruebas_on_reset_password_token", unique: true
 
   create_table "queries", force: true do |t|
     t.string   "nombre"
@@ -88,8 +124,14 @@ ActiveRecord::Schema.define(version: 20141019045612) do
   end
 
   create_table "users", force: true do |t|
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.string   "nombre"
+    t.string   "apellidoPat"
+    t.string   "apellidoMat"
+    t.string   "direccion"
+    t.string   "telefono"
+    t.string   "ciudad"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -100,13 +142,6 @@ ActiveRecord::Schema.define(version: 20141019045612) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "nombre"
-    t.string   "apellidoPaterno"
-    t.string   "apellidoMaterno"
-    t.string   "telefono"
-    t.string   "direccion"
-    t.string   "ciudad"
-    t.integer  "tipo",                   default: 1
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
